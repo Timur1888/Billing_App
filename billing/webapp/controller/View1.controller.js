444422132f1373b1e65fdb68949e621f23976e4c
Timur1888 <timur.kalynovskyi@cto.de>
2025-11-13 14:29:06 +0000

    Table, Button, Icons

--- billing/webapp/controller/View1.controller.js
+++ billing/webapp/controller/View1.controller.js
@@ -5,6 +5,10 @@ sap.ui.define([
 
     return Controller.extend("billing.controller.View1", {
         onInit() {
-        }
+        },
+        onCreate() {},          // folgt später
+        onSearchLive() {},      // folgt später
+        onFilter() {},          // folgt später
+        onSettings() {}         // folgt später (Icon lässt sich dann durch Custom-Icon ersetzen)
     });
 });
