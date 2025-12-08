4f56bb7a63adcadfbe6513f8323e24a0ab50ba80
Timur1888 <timur.kalynovskyi@cto.de>
2025-12-08 13:50:05 +0000

    local credentials

---
+++ billing/webapp/localCredentials.js
@@ -0,0 +1,9 @@
+// webapp/localCredentials.js
+sap.ui.define([], function () {
+  "use strict";
+
+  return {
+    clientId: "Willi",     //  36c323c6a3e36b22280897729a32d85c4b38bde2766d7b0f89aa4632b91a2252
+    clientSecret: "Ecmdemo2025!"   //bb1a956012ac59886de22a089ee90637118861c1bece1157d70e91112a6ba62f
+  };
+});
